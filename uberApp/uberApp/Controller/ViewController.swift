3db936dad363a7b88cb3f58fc77015cd10693b5a
Adrian Garcia <3615924770101@usac.edu.gt>
2024-08-12 16:53:33 -0700

    first navigation controller and addig views

--- uberApp/uberApp/Controller/ViewController.swift
+++ uberApp/uberApp/Controller/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func logInButton(_ sender: UIButton) {
+    }
+    @IBAction func registerButton(_ sender: UIButton) {
+    }
+    
 }
 
