9cab9a4216407f3b6f4d0eb6f1a1c978dc91779d
Adrian Garcia <3615924770101@usac.edu.gt>
2024-08-19 18:51:09 -0700

    Implementing verification screen and new password screen

---
+++ uberApp/uberApp/Controller/newPasswordController.swift
@@ -0,0 +1,28 @@
+//
+//  newPasswordController.swift
+//  uberApp
+//
+//  Created by Adrian Garcia on 19/08/24.
+//
+
+import UIKit
+
+class newPasswordController: UIViewController {
+    
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ uberApp/uberApp/Controller/verificationCodeController.swift
@@ -0,0 +1,37 @@
+//
+//  verificationCodeController.swift
+//  uberApp
+//
+//  Created by Adrian Garcia on 19/08/24.
+//
+
+import UIKit
+
+class verificationCodeController: UIViewController {
+    
+    @IBOutlet weak var firstNumberText: UITextField!
+    @IBOutlet weak var thirdNmberText: UITextField!
+    @IBOutlet weak var secondNumberText: UITextField!
+    @IBOutlet weak var fourthNumberText: UITextField!
+    
+    @IBOutlet weak var verifyButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
